72fd5bf1c8dfe641d50ef34c6b4624aa97a3210e
Quang Luong <quanglddev@gmail.com>
2020-11-02 16:20:47 -0500

    Initial Commit

---
+++ EZMeet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EZMeet
+//
+//  Created by Quang Luong on 11/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
